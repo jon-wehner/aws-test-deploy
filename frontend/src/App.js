c59ff8556690d309fddba5e3361d63fd99c5b046
Jon Wehner <jonjwehner@gmail.com>
2022-07-26 21:55:13 -0700

    docker compose setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello from ur containerino!</p>
+        <p>changed</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
