e39f029f7bfb46316b25d745e939f1ea44d5a36a
Jon Wehner <jonjwehner@gmail.com>
2022-07-27 00:18:02 -0700

    build

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders the changed test', () => {
+  render(<App />);
+  const text = screen.getByText(/changed/i);
+  expect(text).toBeInTheDocument();
+});
