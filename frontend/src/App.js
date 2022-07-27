74a47291c5b856f955a49eb882eec385e07ec4a4
Jon Wehner <jonjwehner@gmail.com>
2022-07-27 03:02:01 -0700

    fix docker compose conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>not changed</p>
+        <p>not Changed</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
